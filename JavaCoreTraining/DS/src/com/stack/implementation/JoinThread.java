b9d2eb576cd1c5598d84eaf0ab374cce30bca611
Bhavya Verma <bhavya.verma@appdirect.com>
2017-02-05 03:16:11 +0530

    Thread

---
+++ JavaCoreTraining/DS/src/com/stack/implementation/JoinThread.java
@@ -0,0 +1,49 @@
+package com.stack.implementation;
+
+/**
+ * Created by bhavyaverma on 2/2/17.
+ */
+public class JoinThread {
+
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MyRunnable(), "t1");
+        Thread t2 = new Thread(new MyRunnable(), "t2");
+        Thread t3 = new Thread(new MyRunnable(), "t3");
+
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        t2.start();
+        t3.start();
+
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Thread starts"+Thread.currentThread().getName());
+
+
+        System.out.println("Thread ends"+Thread.currentThread().getName());
+    }
+
+
+
+
+}
